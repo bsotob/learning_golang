eb534dba81ae9227573c4fae50ef52c62a434416
Borja Soto blanco <borjamacbook@MacBook-Pro-de-Borja.local>
2019-12-04 03:02:42 +0100

    json

--- read_json/main.go
+++ read_json/main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
 
+type Config struct {
+	Usuario    string
+	Contraseña string
+}
+
+func main() {
+	byteValue, err := ioutil.ReadFile("./playbooks/config.json")
+	if err != nil {
+		fmt.Println("ha habido algun problema", err)
+	} else {
+		//fmt.Println(string(file))
+		var config Config
+		json.Unmarshal(byteValue, &config)
+		fmt.Println(string(config.Contraseña))
+		fmt.Println(config)
+	}
+
+	fmt.Println(string(byteValue))
 }
